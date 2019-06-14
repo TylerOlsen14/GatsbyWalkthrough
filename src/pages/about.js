dbc8669d2ea2c7093508b5884c20b5e15411d676
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-06-13 20:32:28 -0600

    Happy with static page changes I've made

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Link } from "gatsby"
 
-const AboutPage = () => {
-  return (
-    <div>
+import Layout from "../components/layout"
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+const AboutPage = () =>(
+    <Layout>
+      <SEO title="about" />
       <h1>About page!!!</h1>
       <h3>This is what we're about:</h3>
       <h6>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat placeat dignissimos numquam sapiente ex tempore, ullam vel quisquam delectus eaque voluptates sed reprehenderit incidunt cum et magnam sunt facere saepe.</h6>
-    </div>
+    </Layout>
   )
-}
 
 export default AboutPage;
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from '../components/seo'
 
 const ServicesPage = () => {
   return (
-    <div>
+    <Layout>
+      <SEO title="about" />
       <h1>Services page!!!</h1>
       <h3>This is what we serve:</h3>
       <h6>What do we do?</h6>
       <p>IDK</p>
-    </div>
+      <p>
+        <Link to="/">Here</Link> is home
+      </p>
+    </Layout>
   )
 }
 
